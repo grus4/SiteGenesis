5fdcdfcbad8a9b7a047c21c335e5649a29d0875e
Rus <rgaiduchok@speroteck.com>
2016-11-28 17:23:16 +0200

    Negative tests for Login form, Create Account form validation_re-register user

--- src/test/java/tests/CreateAccountTests.java
+++ src/test/java/tests/CreateAccountTests.java
@@ -23,10 +23,24 @@ public class CreateAccountTests extends Fixture {
     }
 
     @Test
-    public void emptyFieldsValidationForCreateAccountForm() {
+    public void createAccountFormValidation_emptyFields() {
         siteGenesis.homePage.openPage(SITE_URL);
         siteGenesis.header.switchToCreateAccountPage();
         siteGenesis.createAccountPage.clickApplyButton();
         siteGenesis.createAccountPage.checkValidationForAllEmptyFields();
+    }
+
+    @Test
+    public void createAccountFormValidation_Re_registerTheSameUser() {
+        siteGenesis.homePage.openLoginPage();
+        siteGenesis.header.switchToCreateAccountPage();
+        siteGenesis.createAccountPage.fillFirstNameField(FIRSTNAME);
+        siteGenesis.createAccountPage.fillLastNameField(LASTNAME);
+        siteGenesis.createAccountPage.fillEmailField(EMAIL);
+        siteGenesis.createAccountPage.fillConfirmEmailField(EMAIL);
+        siteGenesis.createAccountPage.fillPasswordField(PASSWORD);
+        siteGenesis.createAccountPage.fillConfirmPasswordField(PASSWORD);
+        siteGenesis.createAccountPage.clickApplyButton();
+        siteGenesis.createAccountPage.checkFormValidation_Re_registerTheSameUser();
     }
 }
