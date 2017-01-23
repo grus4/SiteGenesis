675cfb21623bd44b6ff7d45ec462834dd1d23c7d
Rus <rgaiduchok@speroteck.com>
2017-01-23 14:57:31 +0200

    checkoutAsRegisteredUser_GroundShipping

--- src/test/java/tests/CheckoutRegisteredUser.java
+++ src/test/java/tests/CheckoutRegisteredUser.java
@@ -1,16 +1,16 @@
 package tests;
 
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
+import org.testng.xml.dom.ParentSetter;
 
 /**
  * Created by Rus on 18.01.2017.
  */
 public class CheckoutRegisteredUser extends Fixture {
 
-    //tests looks soo long and unreadable
     @Test
-    public void checkoutAsRegisteredUser_Overnight_Visa() {
+    public void checkoutAsRegisteredUser_OvernightShipping_Visa() {
         siteGenesis.homePage.openPage(SITE_URL);
         siteGenesis.header.switchToCLP();
         siteGenesis.categoryLandingPage.switchToVariationPDP();
@@ -36,6 +36,39 @@ public class CheckoutRegisteredUser extends Fixture {
         siteGenesis.billingPage.selectMonth(MONTH);
         siteGenesis.billingPage.selectYear(YEAR);
         siteGenesis.billingPage.fillSecurityCodeRegisteredUser(VISA_CVV);
+        siteGenesis.billingPage.switchToReviewPage();
+        siteGenesis.reviewPage.switchToOrderReceiptPage();
+        Assert.assertTrue(siteGenesis.orderReceiptPage.isThankYouMessageAvailable(), "Thank you message is not displayed");
+        siteGenesis.header.switchToHomePage();
+    }
+
+    @Test
+    public void checkoutAsRegisteredUser_GroundShipping_MasterCard(){
+        siteGenesis.homePage.openPage(SITE_URL);
+        siteGenesis.header.switchToCLP();
+        siteGenesis.categoryLandingPage.switchToVariationPDP();
+        siteGenesis.variationPDP.addVariationProductToCart();
+        siteGenesis.header.switchToShoppingCart();
+        siteGenesis.shoppingCartPage.switchToLoginCheckoutPage();
+        siteGenesis.checkoutLoginPage.fillEmailField(EMAILFORREGISTEREDUSER);
+        siteGenesis.checkoutLoginPage.fillPasswordField(PASSWORDFORREGISTEREDUSER);
+        siteGenesis.checkoutLoginPage.logInAsRegisteredUser();
+        siteGenesis.shippingPage.fillFirstNameField(FIRSTNAME);
+        siteGenesis.shippingPage.fillLastNameField(LASTNAME);
+        siteGenesis.shippingPage.fillAddressLine_1_Field(ADDRESS_1);
+        siteGenesis.shippingPage.fillAddressLine_2_Field("");
+        siteGenesis.shippingPage.fillCityField(CITY);
+        siteGenesis.shippingPage.fillZipField(ZIP);
+        siteGenesis.shippingPage.selectState(STATE);
+        siteGenesis.shippingPage.fillPhoneField(PHONE);
+        siteGenesis.shippingPage.selectUseTheSameAddressForBillingCheckbox();
+        siteGenesis.shippingPage.switchToBillingPage();
+        siteGenesis.billingPage.fillCardName(CARDNAME_MASTERCARD);
+        siteGenesis.billingPage.selectCardType(CARD_TYPE_2);
+        siteGenesis.billingPage.fillCardNumberRegisteredUser(CARDNUMBER_MASTERCARD);
+        siteGenesis.billingPage.selectMonth(MONTH);
+        siteGenesis.billingPage.selectYear(YEAR);
+        siteGenesis.billingPage.fillSecurityCodeRegisteredUser(MASTERCARD_CVV);
         siteGenesis.billingPage.switchToReviewPage();
         siteGenesis.reviewPage.switchToOrderReceiptPage();
         Assert.assertTrue(siteGenesis.orderReceiptPage.isThankYouMessageAvailable(), "Thank you message is not displayed");
